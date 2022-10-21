fe678f20ed468f99e6a27fd59e2780aef106c41e
FURKAN GUNDUZ <fgunduz211@posta.pau.edu.tr>
2022-10-21 12:48:08 +0300

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Demo_Product.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+        JobManager jobManager = new JobManager(new EfJobDal());
+        public IActionResult Index()
+        {
+            var values = customerManager.GetCustomersListWithJob();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            JobManager jobManager = new JobManager(new EfJobDal());
+
+            List<SelectListItem> value = (from x in jobManager.TGetList()
+                                         select new  SelectListItem
+                                             {
+                                             Text = x.JobName,
+                                             Value = x.JobName
+
+            }).ToList();
+            ViewBag.v = value;
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            customerManager.TAdd(customer);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCustomer(int id)
+        {
+            var result = customerManager.TGetById(id);
+            customerManager.TRemove(result);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            
+            List<SelectListItem> values = (from x in jobManager.TGetList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.JobName,
+                                              Value = x.JobName
+
+                                          }).ToList();
+            ViewBag.v = values;
+            var value = customerManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+       
+
+            customerManager.TUpdate(customer);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation.Results;
+//using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace Demo_Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        public IActionResult Index()
+        {
+            var values = productManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            ProductValidator validator = new ProductValidator();
+            FluentValidation.Results.ValidationResult result = validator.Validate(product);
+            if (result.IsValid) { 
+            productManager.TAdd(product);
+            return RedirectToAction("Index");
+        }
+            else
+            {
+                foreach (var item in result.Errors) {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View(result);
+    }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var result =productManager.TGetById(id);
+            productManager.TRemove(result);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value =productManager.TGetById(id); 
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+           
+            productManager.TUpdate(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
